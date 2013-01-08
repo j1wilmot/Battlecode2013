2ebb6a4ee020de243bb6db26b347f23d35ec30c5
Jeremy Wilmot <j1wilmot@gmail.com>
2013-01-07 23:42:46 -0500

    Initial commit

---
+++ teams/team170/HeadQuarters.java
@@ -0,0 +1,9 @@
+package team170;
+
+import battlecode.common.RobotController;
+
+public class HeadQuarters {
+	public static void run(RobotController rc) {
+
+	}
+}
---
+++ teams/team170/Soldier.java
@@ -0,0 +1,10 @@
+package team170;
+
+import battlecode.common.RobotController;
+
+public class Soldier {
+
+	public static void run(RobotController rc) {
+
+	}
+}
